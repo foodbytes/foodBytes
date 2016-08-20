1cbb73e53385db38c186511ddaf17cc4ba9eedcf
Amanda Baker <mandykatebaker@Amandas-MacBook-Pro.local>
2016-08-21 11:19:41 +1200

    adding audio

--- text-to-audio/script.js
+++ text-to-audio/script.js
@@ -1,20 +1,29 @@
- function playAudio (evt) {
- //
-  if (evt.target.speechRecognitionResult.contains('repeat')) {
-    var commandOne = document.getElementById('repeat-sound')
-    commandOne.volume = 0.5
-    commandOne.play()
-  } else {
-    // You can only mark cells that are hidden!
-    if (evt.target.speechRecognitionResult.contains('next')) {
-      // If the cell wasn't marked, play the mark sound and add a mark!
-      var markSound = document.getElementById('next-sound')
-      commandTwo.volume = 0.5
-      commandTwo.play()
-    }
-  }
 
- //The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
+function play() {
+    document.getElementById('next-sound').play();
+}
+
+// function addListeners (element) {
+//   element.addEventListener('click', playAudio)
+// }
+
+//  function playAudio (evt) {
+//  //
+//   if (evt.target.speechRecognitionResult.contains('repeat')) {
+//     var commandOne = document.getElementById('repeat-sound')
+//     commandOne.volume = 0.5
+//     commandOne.play()
+//   } else {
+//     // You can only mark cells that are hidden!
+//     if (evt.target.speechRecognitionResult.contains('next')) {
+//       // If the cell wasn't marked, play the mark sound and add a mark!
+//       var markSound = document.getElementById('next-sound')
+//       commandTwo.volume = 0.5
+//       commandTwo.play()
+//     }
+//   }
+
+  //The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
   // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
   // It has a getter so it can be accessed like an array
   // The first [0] returns the SpeechRecognitionResult at position 0.
