e49149a01d022d18046fe33fbbd9e7d63e6591d7
AhmedShab <ahmed.vuw@gmail.com>
2016-09-08 11:56:11 +1200

    added the seed file but will need to make few changes

--- database/migrations/20160908102020_createMethodsTable.js
+++ database/migrations/20160908102020_createMethodsTable.js
@@ -2,8 +2,9 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTableIfNotExists('methods', function (table) {
     table.increments('id').primary();
     table.string('name').foreign().references('recipe_name');
-    table.string('text');
-    table.string('audio');
+    table.specificType('text_method', 'text[]');
+    table.specificType('text_audio_steps', 'text[]');
+    table.specificType('audio', 'text[]');
   });
 };
 
