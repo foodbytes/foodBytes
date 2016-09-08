c71597ef68985eea017009654b1091a1dede2498
amaanvaddadi <meetamaanrao@gmail.com>
2016-09-08 14:35:11 +1200

    seeded steps data

--- database/migrations/20160908102020_createMethodsTable.js
+++ database/migrations/20160908102020_createMethodsTable.js
@@ -1,7 +1,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTableIfNotExists('steps', function (table) {
     table.integer('id').primary();
-    table.string('recipe_id').references('id');
+    table.integer('recipe_id').references('id');
     table.string('instructions');
     table.string('audio_path');
   });
--- database/seeds/stepsSeed.js
+++ database/seeds/stepsSeed.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('steps').del()
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('table_name').insert({id: 0, recipe_id: 0, instructions: 'step_one', audio_path: 'rowValue1' }),
-        knex('table_name').insert({id: 1, recipe_id: 0, instructions: 'step_two', audio_path: 'rowValue1'}),
-        knex('table_name').insert({id: 2, recipe_id: 0, instructions: 'step_three', audio_path: 'rowValue1'})
+        knex('steps').insert({id: 0, recipe_id: 0, instructions: 'step_one', audio_path: '../audio/stepOne.mp3' }),
+        knex('steps').insert({id: 1, recipe_id: 0, instructions: 'step_two', audio_path: '../audio/stepTwo.mp3'}),
+        knex('steps').insert({id: 2, recipe_id: 0, instructions: 'step_three', audio_path: '../audio/stepThree.mp3'})
       ]);
     });
 };
