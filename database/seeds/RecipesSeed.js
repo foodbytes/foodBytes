7e357a7c3bad54c92dd2916b5e67ef42ea47fe18
amaanvaddadi <meetamaanrao@gmail.com>
2016-09-08 12:45:56 +1200

    seeded Grilled sandwich recipe

--- database/seeds/RecipesSeed.js
+++ database/seeds/RecipesSeed.js
@@ -1,13 +1,19 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('recipes').del()
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('table_name').insert({id: 1, colName: 'rowValue1'}),
-        knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-        knex('table_name').insert({id: 3, colName: 'rowValue3'})
+        knex('recipes').insert({name: 'Grilled cheese sandwich',
+                                description: 'Indulge in an heavenly melt of cheese grilled in delicious slices',
+                                cooking_time: '10 mins',
+                                ingredients: ['Two slices of cheddar or swiss cheese',
+                                              'Two slices of white bread',
+                                              'Two tablespoons of butter'
+                                             ]
+                              }),
+
       ]);
     });
 };
