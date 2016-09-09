82266ccae892a978ba422465deba3c3467fd0b7e
Martin <martinbutts@clear.net.nz>
2016-09-09 16:39:15 +1200

    basic setup for PREVIOUS test

--- client/reducers/reducer.js
+++ client/reducers/reducer.js
@@ -2,12 +2,13 @@ var reducer = (state, action) => {
   switch (action.type) {
     case 'NEXT':
       if (state.recipe.currentStep === state.recipe.length -1) return state
-      
+
       const newRecipeState = Object.assign({}, state.recipe, {currentStep: state.recipe.currentStep +1})
       const newState = Object.assign({}, state, {recipe: newRecipeState})
       return newState
 
-
+    case 'PREVIOUS':
+      
 
     default:
       return state
