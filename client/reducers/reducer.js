36dfacf5b83e57e78bdc57267e3d4f9c23c6da13
James_B <aesk.99@gmail.com>
2016-09-09 16:39:14 +1200

    reducer tests for RECEIVED RECIPES

--- client/reducers/reducer.js
+++ client/reducers/reducer.js
@@ -1,14 +1,16 @@
-import initialState from './initialstate/initialstate'
+import initialState from '../initialstate/initialstate'
+import {NEXT, PREVIOUS, RECEIVE_RECIPE_STEPS} from '../actions/actionCreators'
 
-export default function reducer (state = initialState, action) {
+export default (state = initialState, action) => {
     switch (action.type){
-      case "RECEIVE_RECIPE_STEPS":
-        let newState = Object.assign({}, action.payload)
+      case RECEIVE_RECIPE_STEPS:
+        let apiData = action.payload
+        let length = apiData.instructions.length
+        let newState = [...state.recipe, apiData]
         console.log('this is the newState ',newState);
-        return newState //change this
-      case "NEXT":
-
+        return Object.assign({}, state, {length: length})
       default:
+        console.log('this is the default');
         return state
     }
 }
