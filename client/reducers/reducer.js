e5e203fe98f026755bdc0638c552864c450d24a5
Martin <martinbutts@clear.net.nz>
2016-09-09 16:00:53 +1200

    added env file

--- client/reducers/reducer.js
+++ client/reducers/reducer.js
@@ -1,10 +1,12 @@
 var reducer = (state, action) => {
   switch (action.type) {
     case 'NEXT':
-
+      if (state.recipe.currentStep === state.recipe.length -1) return state
+      
       const newRecipeState = Object.assign({}, state.recipe, {currentStep: state.recipe.currentStep +1})
       const newState = Object.assign({}, state, {recipe: newRecipeState})
       return newState
+
 
 
     default:
--- client/tests/reducers/nextCase.js
+++ client/tests/reducers/nextCase.js
@@ -26,3 +26,30 @@ test('reducer with basic NEXT action', t=> {
   t.deepEqual(actualState, expectedState, 'the reducer increments the current step')
   t.end()
 })
+
+test('reducer with basic NEXT action when called at end of recipe steps', t=> {
+  var action = {
+    type: 'NEXT'
+  }
+  var initialState = {
+    recipe:{
+      id: 0,
+      currentStep: 4,
+      length: 5
+    }
+  }
+
+  freeze(initialState)
+
+  var expectedState = {
+    recipe:{
+      id: 0,
+      currentStep: 4,
+      length: 5
+    }
+  }
+
+  var actualState = reducer(initialState, action)
+  t.deepEqual(actualState, expectedState, 'the reducer does nothing')
+  t.end()
+})
