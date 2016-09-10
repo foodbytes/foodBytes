b135b75e7a2fcc1035a83dc31bcecb49dd298e3f
amaanvaddadi <meetamaanrao@gmail.com>
2016-09-10 17:44:49 +1200

    added well component and top class of jumbotron styling on contents page

--- client/components/layout/Nav.js
+++ client/components/layout/Nav.js
@@ -6,13 +6,11 @@ class Nav extends Component{
   render(){
 
     return(
-      <ul className="nav nav-tabs">
-        <li role="presentation"><Link to="/">Home</Link></li>
-        <li role="presentation"><Link to="/contents">Table of contents</Link></li>
-        <li role="presentation"><Link to="/recipes">Recipes</Link></li>
-      </ul>
-    );
-  }
-}
 
+          <ul className="well nav nav-tabs">
+            <li role="presentation" className="dropdown"><Link to="/">Home</Link></li>
+            <li role="presentation"><Link to="/contents">Table of contents</Link></li>
+            <li role="presentation"><Link to="/recipes">Recipes</Link></li>
+          </ul>
+        )}}
 export default Nav
