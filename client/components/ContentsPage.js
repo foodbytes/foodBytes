4f20873c532d16cfd60eff0051f930902f2a816f
Amanda Baker <mandykatebaker@Amandas-MacBook-Pro.local>
2016-09-10 14:06:27 +1200

    added a bit more styling

--- client/components/ContentsPage.js
+++ client/components/ContentsPage.js
@@ -10,22 +10,6 @@ class ContentsPage extends Component {
         <div className="page-header">
           <h1 className="text-center">Recipes List</h1>
         </div>
-        <div className="col-sm-6 col-md-6 col-xs-6">
-
-          <div className="thumbnail" style="border:none; background:white;">
-
-            <div className="col-sm-6 col-md-6 col-xs-12 image-container">
-                <img src="images/online_learning.jpg" style="height:200px; margin-left:-15px;"></img>
-            </div>
-
-            <div className="col-sm-6 col-md-6 col-xs-12">
-                <h3>Hello World</h3>
-                <p style="font-size:10px; color:#03225C;"></p>
-            </div>
-          </div>
-
-        </div>
-
       </div>
     )
   }
