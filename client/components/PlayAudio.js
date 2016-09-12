9648626c83827af0d005a95c463d516231bb5029
James_B <aesk.99@gmail.com>
2016-09-13 10:09:32 +1200

    resolved merge conflicts part 2

--- client/components/PlayAudio.js
+++
@@ -1,7 +0,0 @@
-function PlayAudio(props) {
-    console.log("I'm in the PlayAudio", props.data.audio_path[props.data.currentStep - 1])
-    let audio = new Audio(props.data.audio_path[props.data.currentStep - 1])
-    audio.play()
-}
-
-export default PlayAudio
