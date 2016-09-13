1063aa2cb73a5a0f6539cc751a0ff6942732ccea
amaanvaddadi <meetamaanrao@gmail.com>
2016-09-14 11:46:21 +1200

    added functionality to click button on home page

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,42 +1,54 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { fetchRecipe } from '../actions/actionCreators'
+import { fetchRecipe} from '../actions/actionCreators'
 import { Router, Route, hashHistory, Link } from 'react-router'
 import RecipeList from './RecipeList'
+import ReactPlayer from 'react-player'
+
+
 
 class Home extends Component {
 
+  constructor(props){
+
+    super(props)
+    this.handleClickImage= this.handleClickImage.bind(this)
+  }
+
+  handleClickImage(){
+    console.log('bananas');
+    let audio= new Audio("./audio/stepOne.mp3");
+    audio.play()
+  }
+
+
   render () {
     return(
-      <div>
+      <div className="jumbotron">
         <div className="row">
           <div className="page-header text-center">
-            <h1>Food Bytes</h1>
+            <h1>Foodbytes</h1>
           </div>
           <div>
             <h2 className="text-center">Your talking recipe guide</h2>
           </div>
         </div>
+
         <div className="well row">
           <div className="col-xs-12 col-sm-6 col-md-2"></div>
-
-            <div className=" spinnerDiv col-xs-12 col-sm-6 col-md-8 ">
-              <div className=" spinner spinner-4 "></div>
+            <div className=" spinnerDiv col-xs-12 col-sm-6 col-md-8" onClick={this.handleClickImage} id="imageClick">
+              <div className=" spinner spinner-4" data-toggle="collapse" data-target="#demo" ></div>
             </div>
-
-        { /* <div className=" spinnerDiv thumbnail col-xs-12 col-sm-6 col-md-8">
-          <img className="spinner spinner-4" width="400" height="400" src="../../images/bananas_icon_speaker_cartoon.png" alt=""></img>
-          </div>*/}
           <div className="col-lg-12 col-sm-6 col-md-2"></div>
         </div>
-        <div className=" well row">
-          {/*<h2 className="left-text text-center">Why Food Bytes</h2>*/}
-          <p className= "well accordion text-center" data-toggle="collapse" data-target="#demo">Click to know more</p>
-          <div id="demo" className=" text-justify collapse">
-            <h5> Foodbytes simplifies cooking experience by providing a voice interactive recipe instructor that understands commands!. Check the demo on youtube </h5>
+
+        <div className="well row">
+          <div className="col-xs-12 col-sm-6 col-md-2"></div>
+          <div className="well col-xs-12 col-sm-6 col-md-8 text-justify collapse" id="demo" >
+            <h4> Hi and welcome to FoodBytes. I am CC, your guide. Foodbytes is an interactive audio recipe book. By reading the recipes out to you your hands are free to get on with the cooking. To talk to me, say my name and a command. The commands I understand are "CC next", "CC previous" and "CC repeat". So head on over to the recipe page to get started...I will meet you there. </h4>
           </div>
-          <a className="btn btn-danger btn-xs" href="#" role="button">Available Recipes</a>
+          <div className="col-lg-12 col-sm-6 col-md-2"></div>
         </div>
       </div>
     )
