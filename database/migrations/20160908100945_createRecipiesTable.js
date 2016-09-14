16ea932d38689173c0bf2f46444ed36f9b740c48
amaanvaddadi <meetamaanrao@gmail.com>
2016-09-14 16:55:02 +1200

    revert migration change

--- database/migrations/20160908100945_createRecipiesTable.js
+++ database/migrations/20160908100945_createRecipiesTable.js
@@ -2,15 +2,10 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTableIfNotExists('recipes', function (table) {
     table.integer('id').primary();
     table.string('name');
-    table.text('description');
-    table.text('notes');
+    table.string('description');
     table.string('cooking_time');
-    table.text('ingredients');
+    table.string('ingredients');
     table.integer('length');
-    table.text('whole_recipe_audio_path');
-    table.text('ingredients_audio_path');
-    table.text('recipeList_image_path');
-    table.text('recipePage_image_path')
   });
 };
 
