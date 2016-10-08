6371a683d42e293a3a4c71a484cd9645a8d42ded
cam-shotter <cameronshotter@gmail.com>
2016-10-08 19:28:35 +1300

    Dylan and I updated how the test:server script runs and changed the actual and expected tests

--- helpers/test.js
+++ helpers/test.js
@@ -4,46 +4,67 @@ let test = require('tape')
 
 let seedData = [
   {
-    id: 1,
-    name: 'Grilled cheese sandwich',
-    description: 'Indulge in an heavenly melt of cheese grilled in delicious slices',
-    cooking_time: '10 mins',
-    ingredients: 'Two slices of cheddar or swiss cheese,Two slices of white bread,Two tablespoons of butter',
-    recipe_id: 0,
-    instructions: 'step_two',
-    steps_audio_path: '../audio/stepTwo.mp3' },
-   {
     id: 0,
     name: 'Grilled cheese sandwich',
     description: 'Indulge in an heavenly melt of cheese grilled in delicious slices',
+    notes:'Cheddar cheese with brown bread works as well',
     cooking_time: '10 mins',
-    ingredients: 'Two slices of cheddar or swiss cheese,Two slices of white bread,Two tablespoons of butter',
-    recipe_id: 0,
-    instructions: 'step_one',
-    steps_audio_path: '../audio/stepOne.mp3' },
-   {
-    id: 2,
+    ingredients: 'Two slices of cheddar or swiss cheese@Two slices of white bread@Two tablespoons of butter',
+    length: 3,
+    whole_recipe_audio_path:'../audio/grilledCheese/wholeRecipe.mp3',
+    ingredients_audio_path:'../audio/grilledCheese/ingredients.mp3',
+    recipeList_image_path:'../images/grilled_cheese_border_p.png',
+    recipePage_image_path: '../images/grilled_cheese_hushed.png',
+    instructions: 'step one',
+    steps_audio_path: '../audio/stepOne.mp3'
+  },
+  {
+    id: 0,
     name: 'Grilled cheese sandwich',
     description: 'Indulge in an heavenly melt of cheese grilled in delicious slices',
+    notes:'Cheddar cheese with brown bread works as well',
     cooking_time: '10 mins',
-    ingredients: 'Two slices of cheddar or swiss cheese,Two slices of white bread,Two tablespoons of butter',
-    recipe_id: 0,
-    instructions: 'step_three',
-    steps_audio_path: '../audio/stepThree.mp3' }
-
+    ingredients: 'Two slices of cheddar or swiss cheese@Two slices of white bread@Two tablespoons of butter',
+    length: 3,
+    whole_recipe_audio_path:'../audio/grilledCheese/wholeRecipe.mp3',
+    ingredients_audio_path:'../audio/grilledCheese/ingredients.mp3',
+    recipeList_image_path:'../images/grilled_cheese_border_p.png',
+    recipePage_image_path: '../images/grilled_cheese_hushed.png',
+    instructions: 'step two',
+    steps_audio_path: '../audio/stepTwo.mp3'
+  },
+  {
+    id: 0,
+    name: 'Grilled cheese sandwich',
+    description: 'Indulge in an heavenly melt of cheese grilled in delicious slices',
+    notes:'Cheddar cheese with brown bread works as well',
+    cooking_time: '10 mins',
+    ingredients: 'Two slices of cheddar or swiss cheese@Two slices of white bread@Two tablespoons of butter',
+    length: 3,
+    whole_recipe_audio_path:'../audio/grilledCheese/wholeRecipe.mp3',
+    ingredients_audio_path:'../audio/grilledCheese/ingredients.mp3',
+    recipeList_image_path:'../images/grilled_cheese_border_p.png',
+    recipePage_image_path: '../images/grilled_cheese_hushed.png',
+    instructions: 'step three',
+    steps_audio_path: '../audio/stepThree.mp3'
+  }
 ]
 
 let initialState = {
-    id: 0,
-    name: 'Grilled cheese sandwich',
-    description: 'Indulge in an heavenly melt of cheese grilled in delicious slices',
-    ingredients: 'Two slices of cheddar or swiss cheese,Two slices of white bread,Two tablespoons of butter',
-    cooking_time: '10 mins',
-    length: 0,
-    currentStep:0,
-    instructions:['step one', 'step two', 'step three'],
-    steps_audio_path:['../audio/stepOne.mp3', '../audio/stepTwo.mp3', '../audio/stepThree.mp3']
-  }
+  id: 0,
+  name: 'Grilled cheese sandwich',
+  description: 'Indulge in an heavenly melt of cheese grilled in delicious slices',
+  notes:'Cheddar cheese with brown bread works as well',
+  cooking_time: '10 mins',
+  ingredients: 'Two slices of cheddar or swiss cheese@Two slices of white bread@Two tablespoons of butter',
+  length: 3,
+  whole_recipe_audio_path:'../audio/grilledCheese/wholeRecipe.mp3',
+  ingredients_audio_path:'../audio/grilledCheese/ingredients.mp3',
+  recipeList_image_path:'../images/grilled_cheese_border_p.png',
+  recipePage_image_path: '../images/grilled_cheese_hushed.png',
+  instructions:['step one', 'step two', 'step three'],
+  steps_audio_path:['../audio/stepOne.mp3', '../audio/stepTwo.mp3', '../audio/stepThree.mp3']
+}
 
 test('transformJoin transforms a join into initialState',  function (t) {
   const actual = transformJoin(seedData)
