eb9cad925790f5a85dd2d35ef921d0483b38f400
DylanHul <dylan.hulain@gmail.com>
2016-10-09 19:12:26 +1300

    searchbar added needs more styles

--- client/components/layout/Nav.js
+++ client/components/layout/Nav.js
@@ -10,6 +10,16 @@ class Nav extends Component{
         <div className=" navli navbar-default navbar-fixed-top ">
           <Link to="/"><button  role="presentation" className=" nav home btn btn-md">Home</button></Link>
           <Link to="/recipes"><button role="presentation" className=" nav recipes btn btn-md">Recipes</button></Link>
+          <div className=" col-sm-3 col-md-3 pull-right">
+            <form className=" navbar-form" role="search">
+              <div className=" input-group">
+                <input type="text" className=" input-group"placeholder="Search" name="q"/>
+                <div className=" input-group-btn">
+                  <button className=" btn btn-default" type="submit"><i className=" glyphicon glyphicon-search"></i></button>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     )
