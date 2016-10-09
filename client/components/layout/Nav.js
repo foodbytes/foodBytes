cdf075dbaf4523bd76517eb3b09390b9ee1cb03c
DylanHul <dylan.hulain@gmail.com>
2016-10-09 20:26:40 +1300

    sizing fix

--- client/components/layout/Nav.js
+++ client/components/layout/Nav.js
@@ -8,10 +8,10 @@ class Nav extends Component{
 
       <div className="container">
         <div className=" navli navbar-default navbar-fixed-top ">
-          <Link to="/"><button  role="presentation" className=" nav home btn btn-md">Home</button></Link>
-          <Link to="/recipes"><button role="presentation" className=" nav recipes btn btn-md">Recipes</button></Link>
-          <div className=" col-sm-3 col-md-3 pull-right">
-            <form className=" navbar-form" role="search">
+          <Link to="/"><button  role="presentation" className=" nav nav-button btn btn-md">Home</button></Link>
+          <Link to="/recipes"><button role="presentation" className=" nav nav-button btn btn-md">Recipes</button></Link>
+          <div className=" col-xs-3 pull-right">
+            <form className=" navbar-form pull-right" role="search">
               <div className=" input-group">
                 <input type="text" className=" input-group"placeholder="Search" name="q"/>
                 <div className=" input-group-btn">
